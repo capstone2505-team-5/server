3194ec9f2e665d22cf24a08b145fd89126ab1379
Alex Harnett <harnettalex@gmail.com>
2025-07-16 13:43:16 -0700

    Fix for the order of data initialization.

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,13 @@ import fetchRootSpans from "./services/graphqlIngestion/fetchRootSpans";
 
 async function startServer() {
   await initializePostgres();
-  await populateAllMockData();
-  
+
   const rootSpans = await fetchRootSpans();
   if(rootSpans){
     await populateRootSpansTable(rootSpans);
   }
-  
+
+  await populateAllMockData();
   app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
   });
