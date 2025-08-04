4a0696e05fd1113e516c241aa569a0fec5a54011
Noah Raynor <NoahRaynor@gmail.com>
2025-08-04 08:41:16 -0700

    get batch gets all formatted inputs and ouputs

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    setupFiles: ['./tests/setup/testDb.ts'],
+    include: ['tests/**/*.test.ts'],
+    exclude: ['node_modules', 'dist'],
+    testTimeout: 10000,
+    hookTimeout: 10000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@tests': path.resolve(__dirname, './tests'),
+    }
+  }
+})
